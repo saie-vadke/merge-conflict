327c8436721604683aab86d7d55a4ddb99644c59
Saie Suraj vadke <92719904+saie-vadke@users.noreply.github.com>
2023-11-22 11:44:04 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
-pseudo code
-{
-Initial Function()
-{
-  //Sample Code
-}
+# Define the Initial_Function
+def Initial_Function():
+    print("Executing Initial Function")
+    # Sample Code for Initial Function
+    # This could be any functionality or operations you want to perform
 
-Initial Security()
-{
- //Sample Code
-}
+# Define the Initial_Security
+def Initial_Security():
+    print("Executing Initial Security")
+    # Sample Code for Initial Security
+    # This could involve security checks, authentication, or setting up security configurations
 
-
-
-}
+# Calling the functions
+Initial_Function()
+Initial_Security()
